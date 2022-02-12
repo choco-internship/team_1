5c662e292838b2aeb4b6ee119168a1384ab3606c
bixxter <debixxter@gmail.com>
2022-02-12 20:20:31 +0600

    feat: add Header component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,12 @@
 <template>
-  <h1>Главная страница</h1>
+  <Header :isBackIcon="false" :isFlat="true">Заказ с собой</Header>
 </template>
 
 <script>
+import Header from "../components/Header.vue";
 export default {
   name: "Home",
+  components: { Header },
 };
 </script>
 

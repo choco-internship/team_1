8777a6f937599188dcdfefcd2eff3aa448390ec9
bixxter <debixxter@gmail.com>
2022-02-14 03:24:40 +0600

    feat: add layout system

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="main">
+    <slot />
+  </main>
+  <footer class="footer">
+    <Navbar />
+  </footer>
+</template>
+
+<script>
+import Navbar from "../components/Navbar.vue";
+export default {
+  name: "DefaultLayout",
+  components: { Navbar },
+};
+</script>
+
+<style scoped>
+.main {
+  padding-bottom: 56px;
+}
+</style>

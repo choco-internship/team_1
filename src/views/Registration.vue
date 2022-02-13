35c0d6509940c1de6ea8086dc6f188304aaa36ee
tamerlan <remat01@mail.ru>
2022-02-13 12:47:49 +0600

    feat: registration page

--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -1,18 +1,25 @@
 <template>
+  <Header :isBackIcon="true" :isFlat="true"></Header>
   <div class="registration container">
     <form class="form">
       <div class="form__wrapper">
-        <h1 class="title">Регистрация</h1>
-        <p class="form__subtitle">Введите ваш почтовый адрес</p>
-
+        <h1 class="title title--center">
+          {{ formGroup[formPosition].title }}
+        </h1>
+        <p class="form__subtitle">
+          {{ formGroup[formPosition].subtitle }}
+        </p>
         <div class="form__controll">
           <div class="form__input-wrapper">
-            <label class="form__label" for="email">e-mail</label>
+            <label class="form__label" :for="formGroup[formPosition].type">
+              {{ formGroup[formPosition].label }}
+            </label>
             <input
-              id="email"
               class="form__input"
-              type="email"
-              placeholder="Введите почтовый адрес"
+              :id="formGroup[formPosition].type"
+              :type="formGroup[formPosition].type"
+              :placeholder="formGroup[formPosition].placeholder"
+              v-model="formGroup[formPosition].value"
             />
           </div>
         </div>
@@ -22,16 +29,91 @@
         </p>
       </div>
       <div class="form__controll">
-        <BaseButton disabled>Далее</BaseButton>
+        <BaseButton
+          @click.prevent="nextStep(formGroup[formPosition].type)"
+          :disabled="formGroup[formPosition].value ? false : true"
+        >
+          Далее
+        </BaseButton>
       </div>
     </form>
   </div>
 </template>
 
 <script>
+import Header from "@/components/Header.vue";
 import BaseButton from "@/components/BaseButton.vue";
+
 export default {
-  components: { BaseButton },
+  components: { Header, BaseButton },
+  data() {
+    return {
+      formPosition: 0,
+      formGroup: [
+        {
+          type: "email",
+          title: "Регистрация",
+          subtitle: "Введите ваш почтовый адрес",
+          label: "e-mail",
+          placeholder: "Введите ваш почтовый адрес",
+          value: "",
+        },
+        {
+          type: "password",
+          title: "Введите пароль",
+          subtitle: "Пароль должен состоять минимум из 9 символов",
+          label: "Пароль",
+          placeholder: "Введите пароль",
+          value: "",
+        },
+      ],
+    };
+  },
+  methods: {
+    nextStep(type) {
+      const email = this.formGroup[0].value;
+      const password = this.formGroup[1].value;
+
+      switch (type) {
+        case "email":
+          if (!email) {
+            alert("Email required");
+          } else if (!this.validEmail(email)) {
+            alert("Valid email required.");
+            return;
+          }
+          break;
+        case "password":
+          if (!password) {
+            alert("Password required");
+          } else if (!this.validPassword(password)) {
+            alert(
+              "Valid password required. Minimum eight characters, at least one letter and one number."
+            );
+            return;
+          }
+          break;
+        default:
+          break;
+      }
+
+      if (this.formPosition === this.formGroup.length - 1) {
+        alert("success");
+        return;
+      }
+
+      this.formPosition = this.formPosition + 1;
+    },
+    validEmail(email) {
+      const re =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    },
+    validPassword(password) {
+      const re = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+      return re.test(password);
+    },
+  },
 };
 </script>
 
@@ -40,7 +122,7 @@ export default {
   position: relative;
   padding-top: 28px;
   padding-bottom: 23px;
-  height: 100vh;
+  height: calc(100vh - 68px);
 }
 
 .form {
@@ -48,6 +130,10 @@ export default {
   flex-direction: column;
   justify-content: space-between;
   height: 100%;
+}
+
+.title--center {
+  text-align: center;
 }
 
 .form__subtitle {
@@ -83,6 +169,9 @@ export default {
   padding: 14px 16px;
   border: 1px solid #8f8f8f;
   border-radius: 4px;
+  font-size: 16px;
+  line-height: 20px;
+  color: #131113;
   outline: none;
 }
 

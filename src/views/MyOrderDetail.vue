5c153456bdcc4443083f2d5cebe35b0c98572280
tamerlan <remat01@mail.ru>
2022-02-14 22:53:57 +0600

    feat: add myorders page and orders-detail page

---
+++ src/views/MyOrderDetail.vue
@@ -0,0 +1,122 @@
+<template>
+  <Header isBackIcon="true">Детали заказа</Header>
+  <section class="detail main-bg-color">
+    <div class="detail__card container">
+      <h3 class="detail__title">ChocoCafe</h3>
+      <p class="detail__address">1.2 км • Коктем 3-й микрорайон</p>
+      <div class="detail__divider"></div>
+      <div class="detail-block">
+        <p class="detail-block__title">Статус заказа</p>
+        <p class="detail-block__text">Завершен</p>
+      </div>
+      <div class="detail-block">
+        <p class="detail-block__title">Дата</p>
+        <p class="detail-block__text">22.04.2021, 12:43</p>
+      </div>
+      <div class="detail-block">
+        <p class="detail-block__title">Номер заказа</p>
+        <p class="detail-block__text">№3161389</p>
+      </div>
+    </div>
+    <div class="detail__card container">
+      <div class="detail-block">
+        <p class="detail-block__title">Позиции в заказе</p>
+        <div class="detail-block__inner">
+          <span class="detail-block__text">1 х Паста спагетти Алио и Олио</span>
+          <span class="detail-block__text">3 900 тг</span>
+        </div>
+        <div class="detail-block__inner">
+          <span class="detail-block__text">1 х Паста спагетти Алио и Олио</span>
+          <span class="detail-block__text">3 900 тг</span>
+        </div>
+        <div class="detail__divider"></div>
+        <div class="detail__summary">
+          <span>Итого</span>
+          <span>6 900 тг</span>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  components: { Header },
+};
+</script>
+
+<style>
+.detail {
+  background: rgba(248, 248, 248, 255);
+  height: 100vh;
+}
+
+.detail__card {
+  background: #fff;
+  padding-top: 20px;
+  padding-bottom: 16px;
+  margin-bottom: 8px;
+}
+
+.detail__title {
+  font-size: 16px;
+  line-height: 16px;
+  color: #131113;
+  margin-bottom: 12px;
+}
+
+.detail__address {
+  font-size: 14px;
+  line-height: 16px;
+  color: #8f8f8f;
+  margin-bottom: 12px;
+}
+
+.detail__divider {
+  height: 1px;
+  width: 100%;
+  background: #ececec;
+  margin: 12px 0px;
+}
+
+.detail-block {
+  margin-bottom: 16px;
+}
+
+.detail-block:last-child {
+  margin: 0;
+}
+
+.detail-block__title {
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 16px;
+  color: #8f8f8f;
+  margin-bottom: 8px;
+}
+
+.detail-block__text {
+  font-size: 14px;
+  line-height: 16px;
+  color: #131113;
+}
+
+.detail-block__inner {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 12px;
+}
+
+.detail__summary {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 16px;
+  color: #000000;
+}
+</style>
--- src/views/MyOrders.vue
+++ src/views/MyOrders.vue
@@ -1,9 +1,144 @@
 <template>
-  <h1>Мои заказы</h1>
+  <Header>Мои заказы</Header>
+  <section class="order main-bg-color">
+    <div v-if="true" class="order__list">
+      <router-link class="order__link" :to="`/order-detail/${1827402}`">
+        <div class="order-item container">
+          <div class="order-item__header">
+            <h3 class="order-item__title">Mamma mia</h3>
+            <p class="order-item__date">15.12.2021, 10:44</p>
+          </div>
+          <p class="order-item__price">6 090 тг</p>
+          <p class="order-item__status">
+            Статус -
+            <span class="order-item__status-text order-item__status-text--start"
+              >В обработке
+            </span>
+          </p>
+          <img
+            class="order-item__btn"
+            src="@/assets/images/chevron_right.svg"
+            alt=""
+          />
+        </div>
+      </router-link>
+    </div>
+    <div v-else class="order__hidden container">
+      <div class="order__hidden-inner">
+        <h2 class="order__hidden-title">Войдите в свой аккаунт</h2>
+        <p class="order__hidden-subtitle">
+          Необходимо войти в свой аккаунт, чтобы увидеть историю заказов
+        </p>
+        <div class="order__hidden-wrapper">
+          <BaseButton @click="$router.push('/registration')">Войти </BaseButton>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {};
+import BaseButton from "@/components/BaseButton.vue";
+import Header from "@/components/Header.vue";
+
+export default {
+  components: { Header, BaseButton },
+};
 </script>
 
-<style></style>
+<style scoped>
+.order {
+  min-height: 100vh;
+}
+
+.order__list {
+  padding-top: 20px;
+}
+
+.order__link {
+  text-decoration: none;
+}
+
+.order-item {
+  position: relative;
+  padding: 16px;
+  background: #ffffff;
+  margin-bottom: 13px;
+  cursor: pointer;
+}
+
+.order-item__header {
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  margin-bottom: 8px;
+}
+
+.order-item__title {
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 20px;
+  color: #131113;
+}
+
+.order-item__date,
+.order-item__price,
+.order-item__status,
+.order-item__status-text {
+  font-size: 14px;
+  line-height: 16px;
+  color: #8f8f8f;
+}
+
+.order-item__date {
+  margin-right: 36px;
+}
+
+.order-item__status {
+  margin-top: 8px;
+}
+
+.order-item__status-text--start {
+  color: #2997ff;
+}
+
+.order-item__status-text--prepare {
+  color: #e4853d;
+}
+
+.order-item__status-text--ready {
+  color: #51a451;
+}
+
+.order-item__btn {
+  position: absolute;
+  top: 44px;
+  right: 16px;
+}
+
+.order__hidden {
+  padding-top: 162px;
+}
+
+.order__hidden-title {
+  font-size: 18px;
+  line-height: 24px;
+  color: #131113;
+  text-align: center;
+  margin-bottom: 16px;
+}
+
+.order__hidden-subtitle {
+  font-size: 16px;
+  line-height: 20px;
+  color: #131113;
+  text-align: center;
+  margin-bottom: 32px;
+}
+
+.order__hidden-wrapper {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+}
+</style>

5c6b9c85ae5082eb05a8a76afeadcbdcf46a56a2
bixxter <debixxter@gmail.com>
2022-02-27 14:39:49 +0600

    feat: add axios api file

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 import restaurants from "../model/restaurants.model.js";
 import orders from "../model/orders.model.js";
+import auth from "../model/auth.model.js";
 
 const api = axios.create({
-  baseURL: "https://rakhmetapi.herokuapp.com/api",
+  baseURL: "http://142.93.107.238/api",
 });
 
 export default {
   restaurants: restaurants(api),
   orders: orders(api),
+  auth: auth(api),
 };

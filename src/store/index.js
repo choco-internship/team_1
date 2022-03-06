e8413a9aab8ab7e13d2619ee74d2c6c0f80728f0
tamerlan <remat01@mail.ru>
2022-03-07 00:24:18 +0600

    feat: added new actions and mutations

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,15 @@
 import { createStore } from "vuex";
-import api from "../services/api";
+import api from "@/services/api";
 
 export default createStore({
   state() {
     return {
       restaurants: [],
       restaurant: {},
+      orders: {},
+      isAuthenticated: false,
+      isModalOpen: false,
+      isLoading: false,
     };
   },
   mutations: {
@@ -14,6 +18,18 @@ export default createStore({
     },
     SET_RESTAURANT(state, payload) {
       state.restaurant = payload;
+    },
+    SET_ORDERS(state, payload) {
+      state.orders = payload;
+    },
+    SET_IS_AUTHENTICATED(state, payload) {
+      state.isAuthenticated = payload;
+    },
+    SET_IS_MODAL_OPEN(state, payload) {
+      state.isModalOpen = payload;
+    },
+    SET_IS_LOADING(state, payload) {
+      state.isLoading = payload;
     },
   },
   actions: {
@@ -33,9 +49,25 @@ export default createStore({
         console.log("ERROR", error);
       }
     },
+    async FETCH_ORDERS({ commit }, id) {
+      commit("SET_IS_LOADING", true);
+      try {
+        const { data } = await api.orders.getOrder(id);
+        commit("SET_ORDERS", data);
+        commit("SET_IS_LOADING", false);
+      } catch (error) {
+        console.log("ERROR", error);
+      } finally {
+        commit("SET_IS_LOADING", false);
+      }
+    },
   },
   getters: {
     GET_RESTAURANTS: (state) => state.restaurants,
     GET_RESTAURANT: (state) => state.restaurant,
+    GET_ORDERS: (state) => state.orders,
+    GET_IS_AUTHENTICATED: (state) => state.isAuthenticated,
+    GET_IS_MODAL_OPEN: (state) => state.isModalOpen,
+    GET_IS_LOADING: (state) => state.isLoading,
   },
 });

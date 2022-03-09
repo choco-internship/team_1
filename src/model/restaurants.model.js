70e363d7b1f0c62aff635b5128bb7fda63c5ceda
Asatulla <debixxter@gmail.com>
2022-03-09 21:52:56 +0600

    fix: fix order create endpoint

--- src/model/restaurants.model.js
+++ src/model/restaurants.model.js
@@ -2,6 +2,6 @@ export default function (api) {
   return {
     getRestaurants: () => api.get("/restaurants").then(({ data }) => data),
     getRestaurant: (id) => api.get(`/menu/${id}`).then(({ data }) => data),
-    makeOrder: (order) => api.post("/orders/create", order).then((res) => res),
+    makeOrder: (order) => api.post("/orders/", order).then((res) => res),
   };
 }

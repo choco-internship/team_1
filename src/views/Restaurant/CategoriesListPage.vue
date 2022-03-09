90cf551a53f549cc56059e4a16bc109bd3701421
Asatulla <debixxter@gmail.com>
2022-03-09 13:44:53 +0600

    feat: add router-link to categories

--- src/views/Restaurant/CategoriesListPage.vue
+++ src/views/Restaurant/CategoriesListPage.vue
@@ -7,12 +7,23 @@
       :key="i"
     >
       <li class="categories-list__item">
-        <span class="categories-list__name">{{
-          categorie.product_category_name
-        }}</span>
-        <span class="categories-list__amount">{{
-          categorie.products.length
-        }}</span>
+        <router-link
+          class="categories-list__link"
+          :to="{
+            name: 'restaurant',
+            params: {
+              id: restaurant.restaurant_id,
+              selectedCategorie: categorie.product_category_id,
+            },
+          }"
+        >
+          <span class="categories-list__name">{{
+            categorie.product_category_name
+          }}</span>
+          <span class="categories-list__amount">{{
+            categorie.products.length
+          }}</span>
+        </router-link>
       </li>
     </ul>
   </section>
@@ -32,7 +43,7 @@ export default {
     },
   },
   created() {
-    this.$store.dispatch("FETCH_RESTAURANT", this.getId);
+    this.$store.dispatch("fetchRestaurant", this.getId);
   },
 };
 </script>
@@ -46,14 +57,20 @@ export default {
 }
 .categories-list__item {
   width: 100%;
+  padding: 16px;
+  border-top: 1px solid #dbdbdb;
+  list-style: none;
+  cursor: pointer;
+}
+.categories-list__link {
+  width: 100%;
   display: flex;
   justify-content: space-between;
   align-items: center;
   font-size: 16px;
   line-height: 20px;
-  padding: 16px;
-  border-top: 1px solid #dbdbdb;
-  cursor: pointer;
+  text-decoration: none;
+  color: #131113;
 }
 .categories-list__amount {
   color: #8f8f8f;
